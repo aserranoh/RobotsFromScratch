4948097daf9a05af25ff7bdb14d9ff4c7f3d9bc9
Antonio Serrano Hernandez <toni.serranoh@gmail.com>
2019-08-05 11:58:44 +0200

    start developement of ultrasonic module.

---
+++ librfsavr/src/rfsavr/ultrasonic.h
@@ -0,0 +1,36 @@
+/*
+ultrasonic.h - Manages ultrasonic sensors.
+
+This file is part of RobotsFromScratch.
+
+Copyright 2019 Antonio Serrano Hernandez
+
+RobotsFromScratch is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+RobotsFromScratch is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with RobotsFromScratch; see the file COPYING.  If not, see
+<http://www.gnu.org/licenses/>.
+*/
+
+#ifndef RFSAVR_ULTRASONIC_H
+#define RFSAVR_ULTRASONIC_H
+
+void
+rfsavr_ultrasonic_init ();
+
+void
+rfsavr_ultrasonic_trigger ();
+
+int32_t
+rfsavr_ultrasonic_read ();
+
+#endif
+
